d797841d006cbcbc2e7f689eec86e9e78248c639
Andy Anderson <andyanderson1818@gmail.com>
2017-06-27 18:41:40 -0700

    Slide works! TODO: Find out how to capture the acceleration so that when you mouseup, the panels keep sliding

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ let startX;
 let scrollLeft;
 
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseup', () => {
+  slider.classList.remove('active');
   isDown = false;
 });
 slider.addEventListener('mouseleave', () => {
+  slider.classList.remove('active');
   isDown = false;
 });
-slider.addEventListener('mousemove', () => {
+slider.addEventListener('mousemove', (e) => {
   if(!isDown) return //Stop running if mouse is up.
-  console.log(isDown);
-  console.log("Fuck Wes");
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) *3.5;
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
 });
